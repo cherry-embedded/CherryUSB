c0ccbd281d3250bd9ec02a1218b960d2623dd8f0
sakumisu <1203593632@qq.com>
2022-03-11 21:07:42 +0800

    use cdc_acm_template for demo

--- demo/stm32/usb_device/stm32f429igt6/Core/Src/main.c
+++ demo/stm32/usb_device/stm32f429igt6/Core/Src/main.c
@@ -66,121 +66,6 @@ int fputc(int ch, FILE *f)
   return ch;
 }
 
-#define CDC_IN_EP  0x81
-#define CDC_OUT_EP 0x01
-#define CDC_INT_EP 0x82
-
-#define USBD_VID           0xFFFF
-#define USBD_PID           0xFFFF
-#define USBD_MAX_POWER     100
-#define USBD_LANGID_STRING 1033
-
-#define USB_CONFIG_SIZE (9 + CDC_ACM_DESCRIPTOR_LEN)
-
-const uint8_t cdc_descriptor[] = {
-    USB_DEVICE_DESCRIPTOR_INIT(USB_2_0, 0xEF, 0x02, 0x01, USBD_VID, USBD_PID, 0x0100, 0x01),
-    USB_CONFIG_DESCRIPTOR_INIT(USB_CONFIG_SIZE, 0x02, 0x01, USB_CONFIG_BUS_POWERED, USBD_MAX_POWER),
-    CDC_ACM_DESCRIPTOR_INIT(0x00, CDC_INT_EP, CDC_OUT_EP, CDC_IN_EP, 0x02),
-    ///////////////////////////////////////
-    /// string0 descriptor
-    ///////////////////////////////////////
-    USB_LANGID_INIT(USBD_LANGID_STRING),
-    ///////////////////////////////////////
-    /// string1 descriptor
-    ///////////////////////////////////////
-    0x12,                       /* bLength */
-    USB_DESCRIPTOR_TYPE_STRING, /* bDescriptorType */
-    'B', 0x00,                  /* wcChar0 */
-    'o', 0x00,                  /* wcChar1 */
-    'u', 0x00,                  /* wcChar2 */
-    'f', 0x00,                  /* wcChar3 */
-    'f', 0x00,                  /* wcChar4 */
-    'a', 0x00,                  /* wcChar5 */
-    'l', 0x00,                  /* wcChar6 */
-    'o', 0x00,                  /* wcChar7 */
-    ///////////////////////////////////////
-    /// string2 descriptor
-    ///////////////////////////////////////
-    0x24,                       /* bLength */
-    USB_DESCRIPTOR_TYPE_STRING, /* bDescriptorType */
-    'B', 0x00,                  /* wcChar0 */
-    'o', 0x00,                  /* wcChar1 */
-    'u', 0x00,                  /* wcChar2 */
-    'f', 0x00,                  /* wcChar3 */
-    'f', 0x00,                  /* wcChar4 */
-    'a', 0x00,                  /* wcChar5 */
-    'l', 0x00,                  /* wcChar6 */
-    'o', 0x00,                  /* wcChar7 */
-    ' ', 0x00,                  /* wcChar8 */
-    'C', 0x00,                  /* wcChar9 */
-    'D', 0x00,                  /* wcChar10 */
-    'C', 0x00,                  /* wcChar11 */
-    ' ', 0x00,                  /* wcChar13 */
-    'D', 0x00,                  /* wcChar14 */
-    'E', 0x00,                  /* wcChar15 */
-    'M', 0x00,                  /* wcChar16 */
-    'O', 0x00,                  /* wcChar17 */
-    ///////////////////////////////////////
-    /// string3 descriptor
-    ///////////////////////////////////////
-    0x16,                       /* bLength */
-    USB_DESCRIPTOR_TYPE_STRING, /* bDescriptorType */
-    '2', 0x00,                  /* wcChar0 */
-    '0', 0x00,                  /* wcChar1 */
-    '2', 0x00,                  /* wcChar2 */
-    '1', 0x00,                  /* wcChar3 */
-    '0', 0x00,                  /* wcChar4 */
-    '3', 0x00,                  /* wcChar5 */
-    '1', 0x00,                  /* wcChar6 */
-    '0', 0x00,                  /* wcChar7 */
-    '0', 0x00,                  /* wcChar8 */
-    '0', 0x00,                  /* wcChar9 */
-#ifdef CONFIG_USB_HS
-    ///////////////////////////////////////
-    /// device qualifier descriptor
-    ///////////////////////////////////////
-    0x0a,
-    USB_DESCRIPTOR_TYPE_DEVICE_QUALIFIER,
-    0x00,
-    0x02,
-    0x02,
-    0x02,
-    0x01,
-    0x40,
-    0x01,
-    0x00,
-#endif
-    0x00
-};
-
-usbd_class_t cdc_class;
-usbd_interface_t cdc_cmd_intf;
-usbd_interface_t cdc_data_intf;
-
-void usbd_cdc_acm_out(uint8_t ep)
-{
-    uint8_t data[64];
-    uint32_t read_byte;
-    usbd_ep_read(ep,data,64,&read_byte);
-    printf("read len:%d\r\n",read_byte);
-    usbd_ep_read(ep,NULL,0,NULL);
-}
-
-void usbd_cdc_acm_in(uint8_t ep)
-{
-    printf("in\r\n");
-}
-
-usbd_endpoint_t cdc_out_ep = {
-    .ep_addr = CDC_OUT_EP,
-    .ep_cb = usbd_cdc_acm_out
-};
-
-usbd_endpoint_t cdc_in_ep = {
-    .ep_addr = CDC_IN_EP,
-    .ep_cb = usbd_cdc_acm_in
-};
-
 void usb_dc_low_level_init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -208,19 +93,6 @@ void usb_dc_low_level_init(void)
     HAL_NVIC_EnableIRQ(OTG_HS_IRQn);
 }
 
-volatile uint8_t dtr_enable = 0;
-
-void usbd_cdc_acm_set_dtr(bool dtr)
-{
-    if(dtr)
-    {
-        dtr_enable = 1;
-    }
-    else
-    {
-        dtr_enable = 0;
-    }
-}
 /* USER CODE END 0 */
 
 /**
@@ -251,16 +123,13 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  //MX_USB_OTG_HS_PCD_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-  usbd_desc_register(cdc_descriptor);
-  usbd_cdc_add_acm_interface(&cdc_class, &cdc_cmd_intf);
-  usbd_cdc_add_acm_interface(&cdc_class, &cdc_data_intf);
-  usbd_interface_add_endpoint(&cdc_data_intf, &cdc_out_ep);
-  usbd_interface_add_endpoint(&cdc_data_intf, &cdc_in_ep);
+    
+  extern void cdc_acm_init(void);
 
-  usbd_initialize();
-
+  cdc_acm_init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -270,12 +139,9 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-      if(dtr_enable)
-      {
-        uint8_t data_buffer[10] = { 0x31, 0x32, 0x33, 0x34, 0x35, 0x31, 0x32, 0x33, 0x34, 0x35 };
-        usbd_ep_write(CDC_IN_EP, data_buffer, 10, NULL);
-        HAL_Delay(500);
-      }
+    extern void cdc_acm_data_send_with_dtr_test(void);
+    cdc_acm_data_send_with_dtr_test();
+    HAL_Delay(500);
   }
   /* USER CODE END 3 */
 }
