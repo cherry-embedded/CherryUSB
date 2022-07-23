9a46c6c663a1ad812a97824688f8f8fe258e4f25
sakumisu <1203593632@qq.com>
2022-07-23 22:56:27 +0800

    revert it.c that cubemx modified

--- demo/stm32/usb_device/stm32f429igt6/Core/Src/stm32f4xx_it.c
+++ demo/stm32/usb_device/stm32f429igt6/Core/Src/stm32f4xx_it.c
@@ -202,16 +202,16 @@ void SysTick_Handler(void)
 /**
   * @brief This function handles USB On The Go HS global interrupt.
   */
-void OTG_HS_IRQHandler(void)
-{
-  /* USER CODE BEGIN OTG_HS_IRQn 0 */
+//void OTG_HS_IRQHandler(void)
+//{
+//  /* USER CODE BEGIN OTG_HS_IRQn 0 */
 
-  /* USER CODE END OTG_HS_IRQn 0 */
-  HAL_PCD_IRQHandler(&hpcd_USB_OTG_HS);
-  /* USER CODE BEGIN OTG_HS_IRQn 1 */
+//  /* USER CODE END OTG_HS_IRQn 0 */
+//  HAL_PCD_IRQHandler(&hpcd_USB_OTG_HS);
+//  /* USER CODE BEGIN OTG_HS_IRQn 1 */
 
-  /* USER CODE END OTG_HS_IRQn 1 */
-}
+//  /* USER CODE END OTG_HS_IRQn 1 */
+//}
 
 /* USER CODE BEGIN 1 */
 
