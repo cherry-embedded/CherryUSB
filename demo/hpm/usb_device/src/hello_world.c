09c23ff7aa5cc3cb27ee252e88142019103a59c9
sakumisu <1203593632@qq.com>
2022-07-29 19:58:03 +0800

    add hpm device demo

---
+++ demo/hpm/usb_device/src/hello_world.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021 hpmicro
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include <stdio.h>
+#include "board.h"
+#include "hpm_debug_console.h"
+
+#define LED_FLASH_PERIOD_IN_MS 300
+
+int main(void)
+{
+    int u;
+
+    l1c_dc_disable();
+    board_init();
+    board_init_led_pins();
+    board_init_usb_pins();
+
+    board_timer_create(LED_FLASH_PERIOD_IN_MS, board_led_toggle);
+
+    printf("hello world\n");
+
+    extern void cdc_acm_msc_init(void);
+    
+    cdc_acm_msc_init();
+    while(1)
+    {
+        extern void cdc_acm_data_send_with_dtr_test(void);
+        cdc_acm_data_send_with_dtr_test();
+    }
+    return 0;
+}
