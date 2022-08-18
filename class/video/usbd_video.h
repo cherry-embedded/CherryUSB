af39aa39156526a157a86766dc53922ca4174f7d
sakumisu <1203593632@qq.com>
2022-08-18 21:48:42 +0800

    delete sof callback

--- class/video/usbd_video.h
+++ class/video/usbd_video.h
@@ -19,7 +19,6 @@ void usbd_video_close(uint8_t intf);
 void usbd_video_probe_and_commit_controls_init(uint32_t dwFrameInterval, uint32_t dwMaxVideoFrameSize, uint32_t dwMaxPayloadTransferSize);
 uint32_t usbd_video_mjpeg_payload_fill(uint8_t *input, uint32_t input_len, uint8_t *output, uint32_t *out_len);
 void usbd_video_mjpeg_payload_header_toggle(uint8_t *output, uint32_t packets);
-void usbd_video_sof_callback(void);
 
 #ifdef __cplusplus
 }
