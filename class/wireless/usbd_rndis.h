fdfc885fa3a5a7e5f2bbd46cf39eeaa507a1fa40
sakumisu <1203593632@qq.com>
2022-09-17 21:58:27 +0800

    update rndis device driver

--- class/wireless/usbd_rndis.h
+++ class/wireless/usbd_rndis.h
@@ -8,22 +8,22 @@
 
 #include "usb_cdc.h"
 
-#define ETH_HEADER_SIZE 14
-#define RNDIS_MTU            1500 /* MTU value */
+#define ETH_HEADER_SIZE         14
+#define CONFIG_USBDEV_RNDIS_MTU 1500 /* MTU value */
 
-#ifdef CONFIG_USB_HS
-#define RNDIS_LINK_SPEED     12000000 /* Link baudrate (12Mbit/s for USB-FS) */
+#ifndef CONFIG_USB_HS
+#define RNDIS_LINK_SPEED 12000000 /* Link baudrate (12Mbit/s for USB-FS) */
 #else
-#define RNDIS_LINK_SPEED     480000000 /* Link baudrate (480Mbit/s for USB-HS) */
+#define RNDIS_LINK_SPEED 480000000 /* Link baudrate (480Mbit/s for USB-HS) */
 #endif
 
-#define CONFIG_RNDIS_RESP_BUFFER_SIZE 128
+#define CONFIG_USBDEV_RNDIS_RESP_BUFFER_SIZE 128
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void usbd_rndis_add_interface(usbd_class_t *devclass, usbd_interface_t *intf);
+struct usbd_interface *usbd_rndis_alloc_intf(uint8_t int_ep, uint8_t mac[6], uint32_t vendor_id, uint8_t *vendor_desc);
 
 #ifdef __cplusplus
 }
