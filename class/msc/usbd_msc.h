2c541094250a2927d0df64a46fe7973ae64a88f4
sakumisu <1203593632@qq.com>
2022-09-25 19:47:41 +0800

    add msc readonly feature

--- class/msc/usbd_msc.h
+++ class/msc/usbd_msc.h
@@ -15,12 +15,11 @@ extern "C" {
 /* Alloc msc interface driver */
 struct usbd_interface *usbd_msc_alloc_intf(const uint8_t out_ep, const uint8_t in_ep);
 
-void mass_storage_bulk_out(uint8_t ep, uint32_t nbytes);
-void mass_storage_bulk_in(uint8_t ep, uint32_t nbytes);
-
 void usbd_msc_get_cap(uint8_t lun, uint32_t *block_num, uint16_t *block_size);
 int usbd_msc_sector_read(uint32_t sector, uint8_t *buffer, uint32_t length);
 int usbd_msc_sector_write(uint32_t sector, uint8_t *buffer, uint32_t length);
+
+void usbd_msc_set_readonly(bool readonly);
 
 #ifdef __cplusplus
 }
