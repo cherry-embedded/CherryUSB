27ff38f745089844ff97e23bb286771a2af38e4a
sakimisu <1203593632@qq.com>
2022-12-31 14:10:27 +0800

    update video driver

--- class/video/usbh_video.h
+++ class/video/usbh_video.h
@@ -41,6 +41,7 @@ struct usbh_video {
     uint16_t isoin_mps;
     uint16_t isoout_mps;
     bool is_opened;
+    uint8_t current_format;
     uint16_t bcdVDC;
     uint8_t num_of_intf_altsettings;
     uint8_t num_of_formats;
@@ -51,19 +52,27 @@ struct usbh_video {
 extern "C" {
 #endif
 
+void usbh_video_inityuyv2rgb_table(void);
+void usbh_video_yuyv2rgb565(void *input, void *output, uint32_t len);
+
 int usbh_video_get_cur(struct usbh_video *video_class, uint8_t intf, uint8_t entity_id, uint8_t cs, uint8_t *buf, uint16_t len);
 int usbh_video_set_cur(struct usbh_video *video_class, uint8_t intf, uint8_t entity_id, uint8_t cs, uint8_t *buf, uint16_t len);
 int usbh_videostreaming_get_cur_probe(struct usbh_video *video_class);
-int usbh_videostreaming_set_cur_probe(struct usbh_video *video_class, uint8_t formatindex, uint8_t frameindex, uint32_t dwMaxVideoFrameSize, uint32_t dwMaxPayloadTransferSize);
-int usbh_videostreaming_set_cur_commit(struct usbh_video *video_class, uint8_t formatindex, uint8_t frameindex, uint32_t dwMaxVideoFrameSize, uint32_t dwMaxPayloadTransferSize);
+int usbh_videostreaming_set_cur_probe(struct usbh_video *video_class, uint8_t formatindex, uint8_t frameindex);
+int usbh_videostreaming_set_cur_commit(struct usbh_video *video_class, uint8_t formatindex, uint8_t frameindex);
 
-int usbh_video_open(struct usbh_video *video_class, uint8_t altsetting);
+int usbh_video_open(struct usbh_video *video_class,
+                    uint8_t format_type,
+                    uint16_t wWidth,
+                    uint16_t wHeight,
+                    uint8_t altsetting);
 int usbh_video_close(struct usbh_video *video_class);
 
 void usbh_video_list_info(struct usbh_video *video_class);
 
 void usbh_videostreaming_parse_mjpeg(struct usbh_urb *urb, struct usbh_videostreaming *stream);
-void usbh_videostreaming_parse_yuyv2rgb565(struct usbh_urb *urb, struct usbh_videostreaming *stream);
+void usbh_videostreaming_parse_yuyv2(struct usbh_urb *urb, struct usbh_videostreaming *stream);
+void usbh_videostreaming_output(uint8_t *input, uint32_t input_len);
 
 void usbh_video_run(struct usbh_video *video_class);
 void usbh_video_stop(struct usbh_video *video_class);
