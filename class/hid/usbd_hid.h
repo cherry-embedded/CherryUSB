a854ab6fb1bc4474344b0735ab01db829d85e4cb
Chen Leeren <1570139720@qq.com>
2023-07-21 12:12:59 +0800

    Fix the callback api in usbd_hid.c

--- class/hid/usbd_hid.h
+++ class/hid/usbd_hid.h
@@ -20,12 +20,12 @@ void usbd_hid_descriptor_register(uint8_t intf_num, const uint8_t *desc);
 void usbd_hid_report_descriptor_register(uint8_t intf_num, const uint8_t *desc, uint32_t desc_len);
 
 /* Setup request command callback api */
-void usbh_hid_get_report(uint8_t intf, uint8_t report_id, uint8_t report_type, uint8_t **data, uint32_t *len);
-uint8_t usbh_hid_get_idle(uint8_t intf, uint8_t report_id);
-uint8_t usbh_hid_get_protocol(uint8_t intf);
-void usbh_hid_set_report(uint8_t intf, uint8_t report_id, uint8_t report_type, uint8_t *report, uint32_t report_len);
-void usbh_hid_set_idle(uint8_t intf, uint8_t report_id, uint8_t duration);
-void usbh_hid_set_protocol(uint8_t intf, uint8_t protocol);
+void usbd_hid_get_report(uint8_t intf, uint8_t report_id, uint8_t report_type, uint8_t **data, uint32_t *len);
+uint8_t usbd_hid_get_idle(uint8_t intf, uint8_t report_id);
+uint8_t usbd_hid_get_protocol(uint8_t intf);
+void usbd_hid_set_report(uint8_t intf, uint8_t report_id, uint8_t report_type, uint8_t *report, uint32_t report_len);
+void usbd_hid_set_idle(uint8_t intf, uint8_t report_id, uint8_t duration);
+void usbd_hid_set_protocol(uint8_t intf, uint8_t protocol);
 
 #ifdef __cplusplus
 }
