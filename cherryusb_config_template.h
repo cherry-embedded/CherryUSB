b21c45d12b918607ba5ae60f25c7aef7504f3686
sakumisu <1203593632@qq.com>
2024-03-28 20:46:51 +0800

    release v1.2.0

--- cherryusb_config_template.h
+++ cherryusb_config_template.h
@@ -6,8 +6,8 @@
 #ifndef CHERRYUSB_CONFIG_H
 #define CHERRYUSB_CONFIG_H
 
-#define CHERRYUSB_VERSION     0x010100
-#define CHERRYUSB_VERSION_STR "v1.1.0"
+#define CHERRYUSB_VERSION     0x010200
+#define CHERRYUSB_VERSION_STR "v1.2.0"
 
 /* ================ USB common Configuration ================ */
 
