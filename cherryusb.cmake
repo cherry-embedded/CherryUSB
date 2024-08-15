668e41491737393ffb62db5db30421c5d2ed0616
udoudou <949797067@qq.com>
2024-08-15 23:11:45 +0800

    Fix IDF compilation error

--- cherryusb.cmake
+++ cherryusb.cmake
@@ -43,7 +43,7 @@ ${CMAKE_CURRENT_LIST_DIR}/class/vendor/wifi
 
 if(CONFIG_CHERRYUSB_DEVICE)
     list(APPEND cherryusb_srcs ${CMAKE_CURRENT_LIST_DIR}/core/usbd_core.c)
-    if(CONFIG_CHERRYUSB_DEVICE_CDC)
+    if(CONFIG_CHERRYUSB_DEVICE_CDC_ACM)
     list(APPEND cherryusb_srcs ${CMAKE_CURRENT_LIST_DIR}/class/cdc/usbd_cdc.c)
     endif()
     if(CONFIG_CHERRYUSB_DEVICE_HID)
