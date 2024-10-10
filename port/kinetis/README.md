e425b992be1059ab518156f85f6f9ecd2e63fbe0
sakumisu <1203593632@qq.com>
2024-10-10 21:30:42 +0800

    feat(port/kinetis): add kinetis usbip

---
+++ port/kinetis/README.md
@@ -0,0 +1,18 @@
+# Note
+
+## Support Chip List
+
+### NXP
+
+Modify USB_NOCACHE_RAM_SECTION
+
+```
+#define USB_NOCACHE_RAM_SECTION __attribute__((section(".NonCacheable")))
+```
+
+- MCXC/MCXA (device only)
+- MCXN
+
+### MM32
+
+- MM32F3/MM32F5
---
+++ port/kinetis/usb_glue_mcx.c
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2024, sakumisu
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include "usbd_core.h"
+#include "fsl_common.h"
+#include "usb_kinetis_reg.h"
+
+#define USB_OTG_DEV ((KINETIS_MCX_TypeDef *)g_usbdev_bus[busid].reg_base)
+
+void USB0_FS_IRQHandler(void)
+{
+    extern void USBD_IRQHandler(uint8_t busid);
+    USBD_IRQHandler(0);
+}
+
+void USB_ClockInit(void)
+{
+    CLOCK_AttachClk(kCLK_48M_to_USB0);
+    CLOCK_EnableClock(kCLOCK_Usb0Ram);
+    CLOCK_EnableClock(kCLOCK_Usb0Fs);
+    CLOCK_EnableUsbfsClock();
+}
+
+void usb_dc_low_level_init(uint8_t busid)
+{
+    USB_ClockInit();
+    /* Install isr, set priority, and enable IRQ. */
+    NVIC_SetPriority((IRQn_Type)USB0_FS_IRQn, 3);
+    EnableIRQ((IRQn_Type)USB0_FS_IRQn);
+
+    USB_OTG_DEV->USBTRC0 |= USB_USBTRC0_USBRESET_MASK;
+    while (USB_OTG_DEV->USBTRC0 & USB_USBTRC0_USBRESET_MASK)
+        ;
+
+    USB_OTG_DEV->USBTRC0 |= USB_USBTRC0_VREGIN_STS(1); /* software must set this bit to 1 */
+    USB_OTG_DEV->USBCTRL = 0;
+    USB_OTG_DEV->CONTROL |= USB_CONTROL_DPPULLUPNONOTG_MASK;
+}
+
+void usb_dc_low_level_deinit(uint8_t busid)
+{
+    USB_OTG_DEV->CONTROL &= ~USB_CONTROL_DPPULLUPNONOTG_MASK;
+    DisableIRQ((IRQn_Type)USB0_FS_IRQn);
+}
+
+void usbd_kinetis_delay_ms(uint8_t ms)
+{
+
+}
