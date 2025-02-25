db0f5475b4ada7fde20aeb3dd1742c8124cd7db1
sakumisu <1203593632@qq.com>
2025-02-25 22:16:09 +0800

    fix(port/ch32): add EPn_SET_TX_LEN for mps
    Signed-off-by: sakumisu <1203593632@qq.com>

--- port/ch32/usb_ch58x_dc_usbfs.c
+++ port/ch32/usb_ch58x_dc_usbfs.c
@@ -531,6 +531,7 @@ USBD_IRQHandler(void)
                             usb_dc_cfg.ep_in[epid].actual_xfer_len += usb_dc_cfg.ep_in[epid].mps;
                             if (usb_dc_cfg.ep_in[epid].xfer_len > usb_dc_cfg.ep_in[epid].mps) {
                                 memcpy(usb_dc_cfg.ep_in[epid].ep_ram_addr, usb_dc_cfg.ep_in[epid].xfer_buf, usb_dc_cfg.ep_in[epid].mps);
+                                EPn_SET_TX_LEN(epid, usb_dc_cfg.ep_in[epid].mps);
                             } else {
                                 memcpy(usb_dc_cfg.ep_in[epid].ep_ram_addr, usb_dc_cfg.ep_in[epid].xfer_buf, usb_dc_cfg.ep_in[epid].xfer_len);
                                 EPn_SET_TX_LEN(epid, usb_dc_cfg.ep_in[epid].xfer_len);
