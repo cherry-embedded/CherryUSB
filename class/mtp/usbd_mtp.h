b5789f4e687145839598677b53efd62f9cd3a209
sakumisu <1203593632@qq.com>
2026-06-01 20:26:07 +0800

    refactor(class/mtp): remove system stat and fcntl dependence
    Signed-off-by: sakumisu <1203593632@qq.com>

--- class/mtp/usbd_mtp.h
+++ class/mtp/usbd_mtp.h
@@ -7,10 +7,36 @@
 #define USBD_MTP_H
 
 #include "usb_mtp.h"
-#include <sys/stat.h>
-#include <fcntl.h>
 
-/* gcc toolchain does not implement dirent.h, so we define our own MTP_DIR and mtp_dirent */
+#define MTP_O_RDONLY 0 /* +1 == FREAD */
+#define MTP_O_WRONLY 1 /* +1 == FWRITE */
+#define MTP_O_RDWR   2 /* +1 == FREAD|FWRITE */
+
+#define MTP_S_IFMT   0170000 /* type of file */
+#define MTP_S_IFDIR  0040000 /* directory */
+#define MTP_S_IFCHR  0020000 /* character special */
+#define MTP_S_IFBLK  0060000 /* block special */
+#define MTP_S_IFREG  0100000 /* regular */
+#define MTP_S_IFLNK  0120000 /* symbolic link */
+#define MTP_S_IFSOCK 0140000 /* socket */
+#define MTP_S_IFIFO  0010000 /* fifo */
+#define MTP_S_IREAD  0000400 /* read permission, owner */
+#define MTP_S_IWRITE 0000200 /* write permission, owner */
+#define MTP_S_IEXEC  0000100 /* execute/search permission, owner */
+#define MTP_S_ENFMT  0002000 /* enforcement-mode locking */
+
+#define MTP_S_IRWXU (MTP_S_IRUSR | MTP_S_IWUSR | MTP_S_IXUSR)
+#define MTP_S_IRUSR 0000400 /* read permission, owner */
+#define MTP_S_IWUSR 0000200 /* write permission, owner */
+#define MTP_S_IXUSR 0000100 /* execute/search permission, owner */
+#define MTP_S_IRWXG (MTP_S_IRGRP | MTP_S_IWGRP | MTP_S_IXGRP)
+#define MTP_S_IRGRP 0000040 /* read permission, group */
+#define MTP_S_IWGRP 0000020 /* write permission, grougroup */
+#define MTP_S_IXGRP 0000010 /* execute/search permission, group */
+#define MTP_S_IRWXO (MTP_S_IROTH | MTP_S_IWOTH | MTP_S_IXOTH)
+#define MTP_S_IROTH 0000004 /* read permission, other */
+#define MTP_S_IWOTH 0000002 /* write permission, other */
+#define MTP_S_IXOTH 0000001 /* execute/search permission, other */
 
 typedef void MTP_DIR;
 
@@ -18,6 +44,18 @@ struct mtp_statfs {
     size_t f_bsize;  /* block size */
     size_t f_blocks; /* total data blocks in file system */
     size_t f_bfree;  /* free blocks in file system */
+};
+
+struct mtp_stat {
+    uint32_t st_dev;
+    uint32_t st_ino;
+    uint32_t st_mode;
+    uint32_t st_nlink;
+    uint32_t st_uid;
+    uint32_t st_gid;
+    size_t st_size;
+    size_t st_blksize;
+    size_t st_blocks;
 };
 
 struct mtp_dirent {
@@ -49,7 +87,7 @@ int usbd_mtp_closedir(MTP_DIR *d);
 struct mtp_dirent *usbd_mtp_readdir(MTP_DIR *d);
 
 int usbd_mtp_statfs(const char *path, struct mtp_statfs *buf);
-int usbd_mtp_stat(const char *file, struct stat *buf);
+int usbd_mtp_stat(const char *file, struct mtp_stat *buf);
 
 int usbd_mtp_open(const char *path, uint8_t mode);
 int usbd_mtp_close(int fd);
