02acc7c1221e446b3e07d3e3c259da1d4587fc7d
sakumisu <1203593632@qq.com>
2026-09-18 16:12:41 +0800

    chore: add sifli ci
    Signed-off-by: sakumisu <1203593632@qq.com>

--- platform/rtthread/rt_usbh_msc.c
+++ platform/rtthread/rt_usbh_msc.c
@@ -29,9 +29,13 @@ static rt_err_t rt_udisk_init(rt_device_t dev)
 
     return RT_EOK;
 }
-
+#if (RTTHREAD_VERSION >= RT_VERSION_CHECK(5, 0, 0))
 static rt_ssize_t rt_udisk_read(rt_device_t dev, rt_off_t pos, void *buffer,
                                 rt_size_t size)
+#else
+static rt_size_t rt_udisk_read(rt_device_t dev, rt_off_t pos, void *buffer,
+                               rt_size_t size)
+#endif
 {
     struct usbh_msc *msc_class = (struct usbh_msc *)dev->user_data;
     int ret;
@@ -62,8 +66,13 @@ static rt_ssize_t rt_udisk_read(rt_device_t dev, rt_off_t pos, void *buffer,
     return size;
 }
 
+#if (RTTHREAD_VERSION >= RT_VERSION_CHECK(5, 0, 0))
 static rt_ssize_t rt_udisk_write(rt_device_t dev, rt_off_t pos, const void *buffer,
                                  rt_size_t size)
+#else
+static rt_size_t rt_udisk_write(rt_device_t dev, rt_off_t pos, const void *buffer,
+                                rt_size_t size)
+#endif
 {
     struct usbh_msc *msc_class = (struct usbh_msc *)dev->user_data;
     int ret;
