5fc6eae24e154713af4e471e6e45a9841eb7563f
Links0617 <lhd@wch.cn>
2026-09-20 14:52:45 +0800

    fix(port/wch): fixed a bug

--- port/wch/usbhs/usb_hc_usbhs.c
+++ port/wch/usbhs/usb_hc_usbhs.c
@@ -135,7 +135,6 @@ static struct usbhs_pipe *usbhs_pipe_alloc(struct usbh_bus *bus, struct usbh_urb
                 g_usbhs_hcd[bus->hcd.hcd_id].pipe_pool[chidx].interval = 0;
             }
 
-            usb_osal_sem_reset(g_usbhs_hcd[bus->hcd.hcd_id].pipe_pool[chidx].waitsem);
             memset(&g_usbhs_hcd[bus->hcd.hcd_id].pipe_pool[chidx].xfer, 0, sizeof(usbhs_xfer_t));
             g_usbhs_hcd[bus->hcd.hcd_id].pipe_pool[chidx].urb = urb;
             if (g_usbhs_hcd[bus->hcd.hcd_id].pipe_list[type] == NULL) {
